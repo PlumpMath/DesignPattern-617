a9e4864bfef3f613d9d150d605fbd79c41dc6e91
Wen Jian <824636515@qq.com>
2016-08-12 11:40:34 +0800

    状态模式1

--- app/src/main/java/com/douliu/designpattern/MainActivity.java
+++ app/src/main/java/com/douliu/designpattern/MainActivity.java
@@ -9,5 +9,21 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        VendingMachine machine = new VendingMachine(10);
+        machine.insertMoney();
+        machine.backMoney();
+
+        System.out.println("-----------");
+
+        machine.insertMoney();
+        machine.turnCrank();
+
+        System.out.println("----------压力测试-----");
+        machine.insertMoney();
+        machine.insertMoney();
+        machine.turnCrank();
+        machine.turnCrank();
+        machine.backMoney();
+        machine.turnCrank();
     }
 }
